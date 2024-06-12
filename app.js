d23e6a4afeed62c54f2208d61ea07e493ca8e38a
Kartal Akyildiz <akyldzkartal@outlook.com>
2024-06-12 19:17:46 +0300

    animasyonlar ayarlandı

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting){
-            entry.targer.classList.add('show');
-        }
-    });
+const upAnimationElements = document.querySelectorAll("#animation-u, #animation-d, #animation-r, #animation-l, #pop-up");
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("show", entry.isIntersecting);
+            if (entry.isIntersecting) observer.unobserve(entry.target);
+        });
+    },
+    {
+        threshold: 0.3,
+    }
+);
+
+upAnimationElements.forEach(element => {
+    observer.observe(element);
 });
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
-
